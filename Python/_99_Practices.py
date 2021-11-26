8ac966258c1bab7b10d831f184061ba46d08bc7d
Jose Santos <airamez@gmail.com>
2021-11-26 21:32:25 +0000

    Improvements

--- Python/_99_Practices.py
+++ Python/_99_Practices.py
@@ -718,3 +718,16 @@ def isPalindrome(self, s: str) -> bool:
         left += 1
         right -= 1
     return True
+
+# https://leetcode.com/problems/rotate-image
+def rotate(self, matrix: List[List[int]]) -> None:
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(len(matrix)):
+        left = 0
+        right = len(matrix) - 1
+        while left < right:
+            matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
+            left += 1
+            right -= 1
