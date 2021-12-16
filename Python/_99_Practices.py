4707d94c51f560e44e38618099377739fd68cc66
Jose Santos <airamez@gmail.com>
2021-12-15 19:35:44 -0800

    Improvements

--- Python/_99_Practices.py
+++ Python/_99_Practices.py
@@ -1450,3 +1450,32 @@ def findKthLargest(self, nums: List[int], k: int) -> int:
         if len(heap) > k:
             heapq.heappop(heap)
     return heapq.heappop(heap)
+
+# 54. Spiral Matrix
+# https://leetcode.com/problems/spiral-matrix
+def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    top = 0
+    bottom = len(matrix) - 1
+    left = 0
+    right = len(matrix[0]) - 1
+    result = list()
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+        if top > bottom:
+            break
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+        if left > right:
+            break
+        for i in range(right, left - 1, -1):
+            result.append(matrix[bottom][i])
+        bottom -= 1
+        if top > bottom:
+            break
+        for i in range(bottom, top - 1, -1):
+            result.append(matrix[i][left])
+        left += 1
+    return result
